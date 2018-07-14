e551bc7a639ab8ef56e7cc67c3a241b3c22b08eb
UserDev <UserDev>
2018-07-14 19:45:37 +0300

    step 2

--- src/app/stock/stock-item/stock-item.component.ts
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -8,16 +8,38 @@ import { Stock } from '../../model/stock';
 })
 export class StockItemComponent implements OnInit {
   public stock: Stock;
+  public stockClasses;
+  public stockStyles;
+  public stocks: Array<Stock>;
 
   constructor() { }
 
   ngOnInit() {
     this.stock = new Stock('Test Stock', 'TSC', 85, 80);
+    this.stocks = [
+      new Stock('Test stock','TS',85,80),
+      new Stock('Second stock','SS',10,20),
+      new Stock('Last stock','LS',876,765)
+    ];
+
+    let diff = (this.stock.price / this.stock.previousPrice - 1);
+    let largeChange = Math.abs(diff) > 0.01;
+    this.stockClasses= {
+      'positive': this.stock.isPositiveChange,
+      'negative': !this.stock.isPositiveChange,
+      'large-change': largeChange,
+      'small-change': !largeChange
+    };
+    this.stockStyles= {
+      'color': this.stock.isPositiveChange?'green':'red',
+      'font-size': largeChange?'1.2em':'0.8em'
+    };
+
   }
 
-  toggleFavorite(event) {
-    console.log('we are toggling the favorite', event);
-    this.stock.favorite = !this.stock.favorite;
+  toggleFavorite(event, index=0) {
+    console.log('we are toggling the favorite', event,index);
+    this.stocks[index].favorite = !this.stocks[index].favorite;
   }
 
 }
